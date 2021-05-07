6bd226c350742bf30364adc3d67ab5e7c8e08ae9
Abhishek Mogaveera <37222497+Abhishek971999@users.noreply.github.com>
2021-05-07 13:02:37 +0530

    dyamic layout :fire:

--- src/convert.js
+++ src/convert.js
@@ -10,10 +10,78 @@ const postMapFunction = require("./layout/index-page-posts");
 const regex = /\{(.*?)\}/g;
 const moveFrom = config.filePath.postsdir;
 const moveTo = config.filePath.outdir;
+const blogListLayout = config.blogListLayout.postsdir;
+const blogListLayoutFile = config.blogListLayout.file;
 
 //Function to read file asynchronously
 const readFileAsync = async (filename) => {
   return readFile(`${moveFrom}/${filename}`);
+};
+
+const readFileAsyncLayout = async (filename) => {
+  return readFile(`${blogListLayout}/${blogListLayoutFile}`);
+};
+const convertMarkdowntoHtml = (finalMarkup, regex = regex, post) => {
+  const changes = finalMarkup.matchAll(regex);
+  while (true) {
+    const change = changes.next();
+    if (change.done) break;
+    let [replacement, prop] = change.value;
+
+    if (
+      prop.includes("title") ||
+      prop.includes("description") ||
+      prop.includes("body") ||
+      prop.includes("date") ||
+      prop.includes("path")
+    ) {
+      if (post) {
+        if (prop.includes("body")) {
+          finalMarkup = finalMarkup.replace(replacement, post?.body);
+        }
+        if (prop.includes("path")) {
+          finalMarkup = finalMarkup.replace(replacement, post?.path);
+        }
+        if (prop.includes("title")) {
+          finalMarkup = finalMarkup.replace(
+            replacement,
+            post?.attributes?.title
+          );
+        }
+        if (prop.includes("description")) {
+          finalMarkup = finalMarkup.replace(
+            replacement,
+            post?.attributes?.description
+          );
+        }
+        if (prop.includes("date")) {
+          finalMarkup = finalMarkup.replace(
+            replacement,
+            new Date(parseInt(post?.attributes?.date)).toDateString()
+          );
+        }
+      }
+    } else {
+      finalMarkup = finalMarkup.replace(replacement, config[prop]);
+    }
+  }
+  return finalMarkup;
+};
+const convertRegularRegex = async (markupFromExternalFile, files, posts) => {
+  let finalMarkup,
+    result = [];
+  try {
+    const resp = await readFileAsyncLayout(files);
+    markupFromExternalFile = resp.toString("utf-8");
+    finalMarkup = markupFromExternalFile;
+    for (const post of posts) {
+      result.push(convertMarkdowntoHtml(finalMarkup, regex, post));
+    }
+    result = result.join("");
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 //Function to build files asynchronously
@@ -22,7 +90,11 @@ const convertFiles = async (posts) => {
   const files = await fs.promises.readdir(moveFrom);
   for (const file of files) {
     if (file == config.mapPostsTo.fileName) {
-      let mappedPosts = postMapFunction(posts);
+      let mappedPosts = await convertRegularRegex(
+        markupFromExternalFile,
+        file,
+        posts
+      );
       config.posts = mappedPosts;
     }
     readFileAsync(file)
