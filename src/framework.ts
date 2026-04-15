f716ea2cc6e5b9bb95b6fb547da2b7e81394b4b2
Abhishek Mogaveera <37222497+Abhishek971999@users.noreply.github.com>
2026-04-16 02:23:11 +0530

    sync src/framework.ts (no lockfile)

--- src/framework.ts
+++ src/framework.ts
@@ -49,7 +49,7 @@ function createXmlEscape(value: string): string {
     .replace(/&/g, "&amp;")
     .replace(/</g, "&lt;")
     .replace(/>/g, "&gt;")
-    .replace(/\"/g, "&quot;")
+    .replace(/"/g, "&quot;")
     .replace(/'/g, "&apos;");
 }
 
